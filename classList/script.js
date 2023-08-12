751a6486f1b09acf7b95137e088774ef1bdff53e
NISHIYAMA-Takuki <nishiyama.takuki@ai-li.co.jp>
2023-08-12 18:35:21 +0900

    アクセシビリティ対応, 各寸法最適化

--- classList/script.js
+++ classList/script.js
@@ -198,7 +198,7 @@ const lectureDB = {
       return loadedLectureList;
     }
 
-    // 以下、前のシステムのコードを借りました
+    // from former system's code
   
     // テキストの全角英数字, 全角スペース, 空文字を除去する
     // 小文字にはしないので検索時は別途toLowerCase()すること
@@ -256,7 +256,7 @@ const lectureDB = {
       lecture.shortenedCategory = lecture.type + getShortenedCategory(lecture.category);
       lecture.shortenedEvaluation = getShortenedEvaluation(lecture.evaluation);
       if (lecture.shortenedEvaluation === "試験レポ出席平常") {
-        lecture.shortenedEvaluation = "試験レポ<br>出席平常";
+        lecture.shortenedEvaluation = "試験レポ<wbr>出席平常";
       }
     }
 
@@ -622,6 +622,31 @@ const calendar = {
 };
 calendar.init();
 
+// アクセシビリティ対応
+const makeAccessibleByKey = (element, callback=null) => {
+  element.addEventListener('keydown', function(ev) {
+    switch (ev.key) {
+      case " ":
+      case "Enter":
+        this.click();
+        callback?.();
+        this.focus();
+        ev.preventDefault();
+          break;
+      default:
+        break;
+    }
+  });
+};
+window.addEventListener('click', ev => {
+  switch (ev.target?.tagName) {
+    case "INPUT":
+      break;
+    default:
+      ev.target.blur();
+  }
+});
+
 // moduleLike: 検索機能
 
 // init-callback: lectureTable
@@ -643,6 +668,10 @@ const search = {
       this.buttons.update();
       lectureTable.update(this.showRegisteredButton.checked);
     });
+    // TODO: 移動
+    makeAccessibleByKey(document.getElementById("available-only-label"));
+    makeAccessibleByKey(document.getElementById("search-all-label"));
+    makeAccessibleByKey(document.getElementById("registered-lecture-label"));
     // TODO: 可能性: 所管移動
     // 空きコマ選択ボタン
     const selectBlankButton = document.getElementById("blank-period");
@@ -850,12 +879,11 @@ const search = {
 
       checkbox.addEventListener('change', () => {
         search.condition.index[category].set(name, checkbox.checked);
-        benchmark.log(`${category}-${name} -> ${checkbox.checked}`);
         lectureTable.update();
       });
       label.addEventListener('keydown', (ev) => {
         if ((ev.key === " ") || (ev.key === "Enter")) {
-          checkbox.checked ^= true;
+          checkbox.click();
           ev.preventDefault();
         }
       })
@@ -891,12 +919,11 @@ const search = {
     
         radio.addEventListener('change', () => {
           search.condition.index[category].set(name, reaction);
-          benchmark.log(`${category}-${name} -> ${reaction}`);
           lectureTable.update();
         });
         label.addEventListener('keydown', (ev) => {
           if ((ev.key === " ") || (ev.key === "Enter")) {
-            radio.checked = true;
+            radio.click();
             buttonStorage.at(condition.indexOf(reaction)-2).label.focus();
             ev.preventDefault();
           }
